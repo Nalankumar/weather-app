01cbee13715d3afed64e1d18b4a2333a11835ae9
Nalankumar <thirunalankumar@gmail.com>
2024-12-22 11:13:01 +0530

    icons changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Weather from './components/Weather.jsx'
 function App() {
   return (
     
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex flex-col justify-evenly items-center bg-backGround font-poppins">
+      <h1 className="text-3xl font-bold">Weather App</h1>
       <Weather />
     </div>
   
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,9 @@
 import {useState} from 'react'
 import Day from './../assets/weather_svg/static/day.svg'
 import Rainy from './../assets/weather_svg/static/rainy-1.svg'
+import Humidity from './../assets/humidity.svg'
+
+
 export default function Weather(){
 	const url = "https://api.weatherapi.com/v1"
 	const [weather,setWeather] = useState({})
@@ -28,7 +31,7 @@ export default function Weather(){
 	},[])
 
 	return(
-		<div className="flex flex-col items-center justify-evenly rounded-md h-[60vh] w-[90vw] md:h-[70vh] md:w-[40vw] bg-slate-300/75">
+		<div className="flex flex-col items-center justify-evenly rounded-md h-[60vh] w-[90vw] md:h-[70vh] md:w-[40vw] bg-card_bg">
 			<label
 			  htmlFor="city"
 			  className="relative block overflow-hidden border-b border-gray-200 bg-transparent pt-3 focus-within:border-blue-600"
@@ -55,18 +58,19 @@ export default function Weather(){
 				<p className="text-xl font-bold">{city}</p>
 				<p className="font-semibold">{status}</p>
 				<img src={Day} className="" alt="day" />
-				<span>{temp}c</span>
+				<span>{temp}℃</span>
 				
 				
 			</div>
 			<div className="flex justify-between">
 				<div className="flex flex-col items-center p-4 md:p-8">
-						<img src={Rainy} className="h-12 w-12" alt="humidity" />
+						<img src={Humidity} className="h-12 w-12" alt="humidity" />
 						<span>{humidity} %</span>
 						<p className="text-xs">Humidity</p>
 					</div>
 					<div className="flex flex-col items-center p-4 md:p-8">
-						<img src={Rainy} className="h-12 w-12" alt="pressure" />
+						{/*<img src={Rainy} className="h-12 w-12" alt="pressure" />*/}
+						<img   className="h-12 w-12" alt="pressure" src="https://img.icons8.com/color/48/atmospheric-pressure.png" alt="atmospheric-pressure"/>
 						<span>{pressure} in</span>
 						<p className="text-xs">Pressure</p>
 					</div>
@@ -76,23 +80,11 @@ export default function Weather(){
 						<p className="text-xs">Wind Speed</p>
 					</div>
 			</div>
-			
-			{/* Base */}
-
 			<a
 			  className="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
 			onClick={() =>{ getData() }}>
 				Get weather  
 			</a>
-
-{/* Border */}
-
-{/*<a
-  className="inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
-  href="#"
->
-  Download
-</a>*/}
 		</div>
 		);
 }
